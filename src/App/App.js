1af36a87b2fbe1ea739dfbe5b42a605db38786e9
Zoe Ames <zoeames@gmail.com>
2020-05-10 18:13:19 -0500

    setup compelte

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Goats</h2>
+        <button className="btn btn-success"><i class="fas fa-tree fa-3x"></i></button>
+      </div>
+    );
+  }
+}
+
+export default App;
