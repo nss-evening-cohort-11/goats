79efb3f2afa601eb5108fa3267303089f7bdbab5
Zoe Ames <zoeames@gmail.com>
2020-05-11 19:56:45 -0500

    goats are displaying

---
+++ src/components/GoatCoral/GoatCoral.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './GoatCoral.scss';
+
+import Goat from '../Goat/Goat';
+
+class GoatCoral extends React.Component {
+  render() {
+    const { goats } = this.props;
+    const makeGoats = goats.map((goat) => (
+      <Goat goat={goat}/>
+    ));
+
+    return (
+      <div className="GoatCoral d-flex flex-wrap">
+        {makeGoats}
+      </div>
+    );
+  }
+}
+
+export default GoatCoral;
